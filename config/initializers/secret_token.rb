bec617284f587c2ce73d3fe412b0fddafb09f6ea
Esther Muurling <esthermuurling@imac-van-esther.lan>
2013-06-20 19:12:06 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tineke::Application.config.secret_token = '5d8115c3ec3c8d1a7cbcaa74744f738ef3a8b41653b498e74b1d6bf5f0117e53562dc187aff8544f39b2c1199b1da619cfcd3777910551be3e926eb70db46cc2'
